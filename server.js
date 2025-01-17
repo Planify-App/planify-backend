01e38ceaad5d63c2ed608ad8ad824dc55760a294
Edu Nievas <114632025+EDUNI11@users.noreply.github.com>
2025-01-17 22:48:05 +0100

    Server login, register, email verification and different querys

--- server.js
+++ server.js
@@ -4,11 +4,13 @@ const admin = require('firebase-admin');
 const serviceAccount = require("./serviceAccountKey.json");
 const  jp = require('jsonpath')
 const crypto = require('crypto');
-
+const nodemailer = require('nodemailer');
+const fs = require('fs');
+const path = require('path');
+const ejs = require('ejs');
+const { Client, createClient} = require('@libsql/client');
 
 const app = express();
-const port = 3080;
-
 
 app.use(express.json());
 app.use(cors());
@@ -17,13 +19,36 @@ admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
 });
 
-app.listen(port, () => {
-    console.log(`Escuchando en el puerto: ${port}`);
+app.listen(process.env.PORT, () => {});
+
+const tursoClient = createClient({
+    url: process.env.TURSO_URL,
+    authToken: process.env.AUTH_TOKEN,
 });
 
-
-//firebase
 const db = admin.firestore();
+
+const noReplyEmail = 'no-reply@eduni.dev';
+const subjectEmail = 'Verificación de Cuenta - Planify';
+
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.GMAIL_USER,
+        pass: process.env.GMAIL_PASS
+    }
+});
+
+const templatePath = path.join(__dirname, 'plantilla-verificacion.html');
+const htmlTemplate = fs.readFileSync(templatePath, 'utf-8');
+
+const data = {
+    nombre: 'TEST',
+    enlace_verificacion: `https://planify.eduni.dev/verificar?token=1234`
+};
+
+const htmlContent = ejs.render(htmlTemplate, data);
+
 app.get('/api/login/verification', async (req, res) => {
     try {
         const correoHash = HashText(req.body.correo)
@@ -44,33 +69,142 @@ app.get('/api/login/verification', async (req, res) => {
     }
 })
 
-app.post('api/register/registerUser', async (req, res) => {
+app.post('/api/register', async (req, res) => {
     try {
+        const mailOptions = {
+            from: noReplyEmail,
+            to: req.body.correo,
+            subject: subjectEmail,
+            html: htmlContent
+        };
+
         const correoHash = HashText(req.body.correo);
         const infoUsuario = {
             contrasena: HashText(req.body.contrasena),
             correo: req.body.correo,
             nombre: req.body.nombre,
             nombre_usuario: req.body.nombre_usuario,
-            verificacion: req.body.verificacion
+            verificacion: req.body.verificacion,
+            quedadas: []
         }
 
         const usuariosSnapshot = await db.collection('Usuarios').doc(correoHash).get();
         if (usuariosSnapshot.exists) {
             res.json({ status: false, message: "El usuario ya existe" });
-        }else {
+        } else {
             await db.collection('Usuarios').doc(correoHash).set(infoUsuario);
             res.json({ status: true, message: "Usuario registrado" });
-        }
 
-    }catch (error) {
+            await transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                    console.log('Error al enviar el correo: ', error);
+                } else {
+                    console.log('Correo enviado: ' + info.response);
+                }
+            });
+        }
+    } catch (error) {
         console.error("Error al registrar usuario:", error);
         res.status(500).json({ error: "Error al registrar usuario" });
     }
 });
 
-// Hashear textos
+app.get('/api/getQuedadasUser', async (req, res) => {
+    try {
+        const correoHash = HashText(req.body.correo);
+        const usuariosSnapshot = await db.collection('Usuarios').doc(correoHash).get();
+        const quedadas = usuariosSnapshot.data().quedadas;
+        const quedadasFinal = await Promise.all(
+            quedadas.map(async (idQuedada) => {
+                const query = `SELECT id, nombre_quedada, descripcion_quedada,fecha_hora_inicio,link_imagen FROM quedadas WHERE id = ?`;
+                try {
+                    const result = await tursoClient.execute({
+                        sql: query,
+                        args: [idQuedada],
+                    })
+                    return result.rows.length > 0 ? result.rows[0] : null;
+                } catch (error) {
+                    console.error(`Error al obtener la quedada ${idQuedada}: ` + error);
+                    return null;
+                }
 
+            })
+        );
+        res.json(quedadasFinal.filter((q) => q !== null));
+    } catch (error) {
+        res.status(error)
+    }
+});
+
+app.get('/api/getQuedadaById', async (req, res) => {
+    try {
+        const idQuedada = req.body.id;
+        const query = `SELECT * FROM quedadas WHERE id = ?`;
+        const result = await tursoClient.execute({
+            sql: query,
+            args: [idQuedada],
+        })
+        console.log(result.rows)
+        res.json(result.rows);
+    } catch (error) {
+        res.status(error)
+    }
+});
+
+app.get('/api/getEventosUser', async (req, res) => {
+    try {
+        const correoHash = HashText(req.body.correo);
+        const usuariosSnapshot = await db.collection('Usuarios').doc(correoHash).get();
+        const quedadas = usuariosSnapshot.data().quedadas;
+        const eventos = await Promise.all(
+            quedadas.map(async (idQuedada) => {
+                const query = `SELECT * FROM eventos WHERE id_quedada = ?`;
+                try {
+                    const result = await tursoClient.execute({
+                        sql: query,
+                        args: [idQuedada],
+                    })
+                    return result.rows.length > 0 ? result.rows : null;
+                } catch (error) {
+                    console.error(`Error al obtener el evento ${idQuedada}: ` + error);
+                    return null;
+                }
+
+            })
+        );
+        res.json(eventos.filter(evento => evento !== null));
+    } catch (error) {
+        res.status(error)
+    }
+});
+
+app.get('/api/getUsersQuedada', async (req, res) => {
+    try {
+        const idQuedada = req.body.id;
+
+        const query = `SELECT id_usuario FROM usuarios_quedada WHERE id_quedada = ?`;
+        const result = await tursoClient.execute({
+            sql: query,
+            args: [idQuedada],
+        })
+
+        const usuariosQuedada = await Promise.all(
+            result.rows.map(async (idUsuario) => {
+                try {
+                    const usuariosSnapshot = await db.collection('Usuarios').doc(idUsuario).get();
+                    return usuariosSnapshot.exists ? usuariosSnapshot.data() : null;
+                } catch (error) {
+                    console.error(`Error al obtener el usuario ${idUsuario}: ` + error);
+                    return null;
+                }
+
+            })
+        );
+        res.json(usuariosQuedada);
+    } catch (error) {
+        res.status(error)
+    }
+});
 
 function HashText(correo){
     const hash = crypto.createHash('sha256');
